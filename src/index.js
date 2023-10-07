c8a68784b0b563662ef08e07c0bff376a43db519
mike-charity <Mikecharity0@gmail.com>
2023-10-07 07:27:44 -0400

    updated link

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import Weather from "./Weather";
-import App from "./App";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <h1> Weather </h1>
     <Weather city="paris" />
-    <App />
+    <footer>
+      This project was coded by{" "}
+      <a href=" https://www.shecodes.io/" target="_blank">
+        charity
+      </a>{" "}
+      and is{" "}
+      <a href="https://github.com/mike-charity/weather" target="_blank">
+        open-sourced on Github
+      </a>
+    </footer>
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
